a62e5c5bc47fe9c732a279e53fb3c5e6ec23a9e3
Nikoleta Papoutsi <nikoletapapoutsi19@gmail.com>
2025-11-21 23:58:53 +0200

    New

--- triptrail-api/src/config/constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/triptrail',
-  PORT: process.env.PORT || 3000,
-  DEFAULT_BUDGET: 1000,
-  DEFAULT_ACTIVITY_TIME: '09:00',
-};
