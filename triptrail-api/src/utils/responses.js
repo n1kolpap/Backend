10832955bc93a83bce9f10c094ffef2fdd1674a5
Nikoleta Papoutsi <nikoletapapoutsi19@gmail.com>
2025-11-21 19:34:09 +0200

    new

--- triptrail-api/src/utils/responses.js
+++ triptrail-api/src/utils/responses.js
@@ -1,4 +1,3 @@
-
 export const successResponse = (res, data, message = "Operation successful") => {
     return res.status(200).json({
         success: true,
@@ -33,22 +32,6 @@ export const notFoundResponse = (res, message = "Resource not found") => {
 export const deleteResponse = (res, message = "Resource deleted") => {
     return res.status(204).json({
         success: true,
-        message,
-    });
-};
-
-export const handleError = (res, error, statusCode = 500, message = "Internal Server Error") => {
-    return res.status(statusCode).json({
-        success: false,
-        error: process.env.NODE_ENV === 'development' ? error.message : message,
-        message,
-    });
-};
-
-export const handleResponse = (res, data, statusCode = 200, message = "Success") => {
-    return res.status(statusCode).json({
-        success: true,
-        data,
         message,
     });
 };
